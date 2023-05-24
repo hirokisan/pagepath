1de7dee706390c1c64b80f9bf990456de3a03714
hirokisan <dev@hirokiueno.com>
2023-05-24 20:45:00 +0900

    test page

---
+++ page_path_test.go
@@ -0,0 +1,33 @@
+package pagepath
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPage_New(t *testing.T) {
+	pagePath := "https://pkg.go.dev/net/url?xxx=bbb#Parse"
+	page, err := New(pagePath)
+	require.NoError(t, err)
+
+	assert.Equal(t, "https", page.Scheme)
+	assert.Equal(t, "pkg.go.dev", page.Host)
+	assert.Equal(t, "/net/url", page.Path)
+	assert.Equal(t, "xxx=bbb", page.Query)
+	assert.Equal(t, "Parse", page.Fragment)
+
+	assert.Equal(t, pagePath, page.String())
+}
+
+func TestPage_Extract(t *testing.T) {
+	page, err := New("https://pkg.go.dev/net/url?xxx=bbb#Parse")
+	require.NoError(t, err)
+
+	assert.Equal(t, "https", page.Extract(ElementScheme))
+	assert.Equal(t, "pkg.go.dev", page.Extract(ElementHost))
+	assert.Equal(t, "/net/url", page.Extract(ElementPath))
+	assert.Equal(t, "xxx=bbb", page.Extract(ElementQuery))
+	assert.Equal(t, "Parse", page.Extract(ElementFragment))
+}
